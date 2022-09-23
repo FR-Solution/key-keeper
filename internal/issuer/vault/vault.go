5e197af1725cbdc60f884a62c3da17b879145a30
irbgeo <morochevgm@gmail.com>
2022-09-23 15:33:38 +0300

    refactoring

--- internal/issuer/vault/vault.go
+++ internal/issuer/vault/vault.go
@@ -4,12 +4,8 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"path"
-	"time"
 
 	"github.com/hashicorp/vault/api"
-	auth "github.com/hashicorp/vault/api/auth/approle"
-	"go.uber.org/zap"
 
 	"github.com/fraima/key-keeper/internal/config"
 	"github.com/fraima/key-keeper/internal/controller"
@@ -57,115 +53,7 @@ func Connect(name string, cfg config.Vault) (controller.Issuer, error) {
 		certificate: make(map[string]config.Certificate),
 		secret:      make(map[string]config.Secret),
 	}
-
-	roleID, err := s.roleID(name, cfg.Auth.AppRole)
-	if err != nil {
-		return nil, fmt.Errorf("get role id: %w", err)
-	}
-	secretID, err := s.secretID(name, cfg.Auth.AppRole)
-	if err != nil {
-		return nil, fmt.Errorf("get secret id: %w", err)
-	}
-
-	appRoleAuth, err := auth.NewAppRoleAuth(
-		roleID,
-		&auth.SecretID{
-			FromString: secretID,
-		},
-		auth.WithMountPath(cfg.Auth.AppRole.Path),
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	authInfo, err := client.Auth().Login(context.Background(), appRoleAuth)
-	if err != nil {
-		return nil, err
-	}
-	if authInfo == nil {
-		return nil, fmt.Errorf("no auth info was returned after login")
-	}
-
-	go s.authWorker(authInfo)
-	return s, nil
-}
-
-func (s *vault) roleID(name string, appRole config.AppRole) (string, error) {
-	if roleID, rErr := readFromFile(appRole.RoleIDLocalPath); rErr == nil {
-		return string(roleID), nil
-	}
-
-	vaultPath := path.Join("auth", appRole.Path, "role", appRole.Name, "role-id")
-	approle, err := s.Read(vaultPath)
-	if err != nil {
-		return "", fmt.Errorf("read role_id for path: %s : %w", vaultPath, err)
-	}
-	if approle == nil {
-		return "", fmt.Errorf("no role_id info was returned")
-	}
-
-	roleID, ok := approle["role_id"]
-	if !ok {
-		return "", fmt.Errorf("not found role_id")
-	}
-
-	if err = writeToFile(appRole.RoleIDLocalPath, []byte(roleID.(string))); err != nil {
-		return "", fmt.Errorf("save role id path: %s : %w", appRole.RoleIDLocalPath, err)
-	}
-	return roleID.(string), err
-}
-
-func (s *vault) secretID(name string, appRole config.AppRole) (string, error) {
-	if secretID, rErr := readFromFile(appRole.SecretIDLocalPath); rErr == nil {
-		return string(secretID), nil
-	}
-
-	vaultPath := path.Join("auth", appRole.Path, "role", appRole.Name, "secret-id")
-	approle, err := s.Write(vaultPath, nil)
-	if err != nil {
-		return "", fmt.Errorf("read secrete_id for path: %s : %w", vaultPath, err)
-	}
-	if approle == nil {
-		return "", fmt.Errorf("no secrete_id info was returned")
-	}
-
-	secretID, ok := approle["secret_id"]
-	if !ok {
-		return "", fmt.Errorf("not found secrete_id")
-	}
-
-	if err = writeToFile(appRole.SecretIDLocalPath, []byte(secretID.(string))); err != nil {
-		return "", fmt.Errorf("save secret id path: %s : %w", appRole.SecretIDLocalPath, err)
-	}
-	return secretID.(string), err
-}
-
-func (s *vault) authWorker(auth *api.Secret) {
-	token, err := auth.TokenID()
-	if err != nil {
-		zap.L().Error("get token from vault", zap.Error(err))
-	}
-	s.cli.SetToken(token)
-
-	ttl, err := auth.TokenTTL()
-	if err != nil {
-		zap.L().Error("get token ttl from vault", zap.Error(err))
-	}
-
-	t := time.NewTimer(ttl)
-	for range t.C {
-		token, err := auth.TokenID()
-		if err != nil {
-			zap.L().Error("get token from vault", zap.Error(err))
-		}
-		s.cli.SetToken(token)
-
-		ttl, err := auth.TokenTTL()
-		if err != nil {
-			zap.L().Error("get token ttl from vault", zap.Error(err))
-		}
-		t.Reset(ttl)
-	}
+	return s, s.auth(name, cfg.Auth)
 }
 
 // Read secret from vault by path.
